5ac98e5d5c2a82f7ea845ef7fb4725e148e99469
YaswanthShankar <67797944+YaswanthShankar@users.noreply.github.com>
2021-01-18 19:02:13 +0530

    Update connection.php

--- php files/connection.php
+++ php files/connection.php
@@ -1,7 +1,7 @@
 <?php
 $username = "iac-dam";
 $pass = "i@c-d@m";
-$host = "172.16.5.233";
+$host = "172.16.knk233";
 $db_name = "iac-dam";
 $db = mysqli_connect($host, $username, $pass,$db_name) or die(mysqli_error());
 ?>
